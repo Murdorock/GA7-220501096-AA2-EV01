39c030aaa6f5a54fc67639bc3cda1785208dea9a
Murdorock <diegmau84@gmail.com>
2024-04-29 20:19:14 -0500

    first commit

---
+++ src/modulos/Consultar.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modulos;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Guigo
+ */
+public class Consultar {
+    
+    public static void main (String[] args) {
+        
+        String usuario="root";
+        String password="murdock1984-";
+        String url="jdbc:mysql://localhost:3306/control";
+        Connection conexion;
+        Statement statement;
+        ResultSet rs;
+        
+        try {
+        
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            conexion= DriverManager.getConnection(url,usuario,password);
+            statement= conexion.createStatement();
+            
+            rs = statement.executeQuery("SELECT * FROM CLIENTES");
+            rs.next();
+            
+            do{
+                System.out.println(rs.getInt("IDCLIENTES")+ ":"+ rs.getString("CEDULA")+":"+ rs.getString("NOMBRE")+":"+ rs.getString("APELLIDO")+ ":"+ rs.getString("CORREO") + ":"+ rs.getString("TELEFONO"));
+                } while (rs.next());
+        }
+        catch (ClassNotFoundException | SQLException ex){
+                ex.printStackTrace();
+                }    
+    }
+    
+}
+
+
